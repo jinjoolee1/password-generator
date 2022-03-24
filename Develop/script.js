ca76f6d80b650ced38b0fe7e311098f93a3fb926
jinjoolee1 <jinjoolee126@gmail.com>
2022-03-24 12:27:48 -0500

    arrays added

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,14 @@
 // Assignment code here
-
-
+upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+numericValue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+specialCharacter = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "~", "?", ]
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var choices = []
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
