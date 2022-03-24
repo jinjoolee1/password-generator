fece50eead46d2352b7cc5296f715f44c2f49b84
jinjoolee1 <jinjoolee126@gmail.com>
2022-03-24 12:37:11 -0500

    index path fixed

--- Develop/script.js
+++ Develop/script.js
@@ -2,13 +2,15 @@
 upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 numericValue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-specialCharacter = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "~", "?", ]
+specialCharacter = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "~", "?", ":", ]
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var choices = []
+  var passwordLength = window.prompt("Password length must be between 8 and 128 characters.");
+    console.log(passwordLength);
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
